f59da031827cfbb05be9035150ec33fb8510025d
Tony <Tony-Mc@users.noreply.github.com>
2020-02-19 01:50:54 +0200

    It's a directory...

--- prometheus/uninstall.sh
+++ prometheus/uninstall.sh
@@ -3,7 +3,7 @@ systemctl disable prometheus
 rm /etc/systemd/system/prometheus.service
 systemctl daemon-reload
 systemctl reset-failed
-rm /opt/prometheus
+rm -rf /opt/prometheus
 
 iptables -D INPUT -i eth0 -p tcp -m tcp --dport 9090 -j ACCEPT
 iptables -D OUTPUT -o eth0 -p tcp -m tcp -m tcp --sport 9090 -m state --state RELATED,ESTABLISHED -j ACCEPT
