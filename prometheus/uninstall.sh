1ebe85ecaf3c339a37ae1c32eb9b8dc86a67a9b6
Tony <Tony-Mc@users.noreply.github.com>
2020-02-19 01:47:07 +0200

    Cleaning iptables as well

--- prometheus/uninstall.sh
+++ prometheus/uninstall.sh
@@ -4,3 +4,8 @@ rm /etc/systemd/system/prometheus.service
 systemctl daemon-reload
 systemctl reset-failed
 rm /opt/prometheus
+
+iptables -D INPUT -i eth0 -p tcp -m tcp --dport 9090 -j ACCEPT
+iptables -D OUTPUT -o eth0 -p tcp -m tcp -m tcp --sport 9090 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -D OUTPUT -o eth0 -p tcp -m tcp --dport 9101 -j ACCEPT
+iptables -D INPUT -i eth0 -p tcp -m tcp -m tcp --sport 9101 -m state --state RELATED,ESTABLISHED -j ACCEPT
