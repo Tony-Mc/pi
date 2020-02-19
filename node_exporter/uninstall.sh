60e9219434ee336b456e46599241f6e52fa7f7ec
Tony <Tony-Mc@users.noreply.github.com>
2020-02-19 02:18:00 +0200

    Fixed a few bugs

--- node_exporter/uninstall.sh
+++ node_exporter/uninstall.sh
@@ -3,4 +3,7 @@ systemctl disable node_exporter
 rm /etc/systemd/system/node_exporter.service
 systemctl daemon-reload
 systemctl reset-failed
-rm /opt/node_exporter
+rm -rf /opt/node_exporter
+
+iptables -D INPUT -i eth0 -p tcp -m tcp --dport 9100 -j ACCEPT
+iptables -D OUTPUT -o eth0 -p tcp -m tcp -m tcp --sport 9100 -m state --state RELATED,ESTABLISHED -j ACCEPT
