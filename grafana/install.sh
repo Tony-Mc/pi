47e397a63a83f89e1e5085a8a27f78c066d2077c
Tony <Tony-Mc@users.noreply.github.com>
2020-02-19 02:45:28 +0200

    Fixed for correct architecture

--- grafana/install.sh
+++ grafana/install.sh
@@ -7,9 +7,9 @@ grafana_version="6.6.1"
 cd /opt
 
 apt-get install -y adduser libfontconfig1
-wget "https://dl.grafana.com/oss/release/grafana_${grafana_version}_armhf.deb"
-dpkg -i "grafana_${grafana_version}_armhf.deb"
-rm "grafana_${grafana_version}_armhf.deb"
+wget "https://dl.grafana.com/oss/release/grafana_${grafana_version}_arm64.deb"
+dpkg -i "grafana_${grafana_version}_arm64.deb"
+rm "grafana_${grafana_version}_arm64.deb"
 
 iptables -A INPUT -i eth0 -p tcp -m tcp --dport 3000 -j ACCEPT
 iptables -A OUTPUT -o eth0 -p tcp -m tcp -m tcp --sport 3000 -m state --state RELATED,ESTABLISHED -j ACCEPT
